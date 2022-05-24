5d136094828b0f84863018e008661fee98437058
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-05-24 15:51:46 -0300

    indent codebase with 2 spaces

--- lua/ft_header/util.lua
+++ lua/ft_header/util.lua
@@ -2,64 +2,59 @@ local M = {}
 local api = vim.api
 
 function M.ft_set_text(line, col, text)
-    if type(text) == "string"
-        then
-        api.nvim_buf_set_text(0, line, col, line, col + string.len(text) , {text})
-    else
-        print(text)
-    end
+  if type(text) == "string" then
+    api.nvim_buf_set_text(0, line, col, line, col + string.len(text), { text })
+  else
+    print(text)
+  end
 end
 
 function M.get_user()
-    return {
-        name = os.getenv("USER42") or "marvin",
-        mail = os.getenv("MAIL42") or "<marvin@42.fr>"
-    }
+  return {
+    name = os.getenv "USER42" or "marvin",
+    mail = os.getenv "MAIL42" or "<marvin@42.fr>",
+  }
 end
 
 function M.makeline(ops, width, is_fill)
-    local res   = ops.start
-    local endlen = string.len(ops.s_end) + 1
-    local stlen = string.len(ops.start)
-    local fill = " "
-    if is_fill
-        then
-        res = res .. " "
-        stlen = stlen + 1
-        fill = ops.fill
-    end
+  local res = ops.start
+  local endlen = string.len(ops.s_end) + 1
+  local stlen = string.len(ops.start)
+  local fill = " "
+  if is_fill then
+    res = res .. " "
+    stlen = stlen + 1
+    fill = ops.fill
+  end
 
-    for _ = stlen + 1,width - endlen
-    do
-        res = res .. fill
-    end
+  for _ = stlen + 1, width - endlen do
+    res = res .. fill
+  end
 
-    return res .. " " .. ops.s_end
+  return res .. " " .. ops.s_end
 end
 
 function M.getline(index)
-    return api.nvim_buf_get_lines(0, index - 1, index, false)[1]
+  return api.nvim_buf_get_lines(0, index - 1, index, false)[1]
 end
 
 function M.prepend_lines(lines)
-    api.nvim_buf_set_lines(0, 0, 0, false, lines)
+  api.nvim_buf_set_lines(0, 0, 0, false, lines)
 end
 
 function M.merge_tbls(t1, t2)
-    for k,v in pairs(t2)
-    do
-        if t1[k] ~= nil
-        then
-            t1[k] = v
-        end
+  for k, v in pairs(t2) do
+    if t1[k] ~= nil then
+      t1[k] = v
     end
-    return t1;
+  end
+  return t1
 end
 
 function M.show_error(msg)
-    vim.cmd("echohl Error")
-    print(msg)
-    vim.cmd("echohl clear")
+  vim.cmd "echohl Error"
+  print(msg)
+  vim.cmd "echohl clear"
 end
 
 return M
